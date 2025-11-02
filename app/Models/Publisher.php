b8aa0bfc185f03c88f455116657c3339f2e60832
Pancosta <dz6.gustav@gmail.com>
2025-11-02 13:45:50 -0300

    correcao 5.2.2ç

--- app/Models/Publisher.php
+++ app/Models/Publisher.php
@@ -1,13 +1,16 @@
 <?php
+    namespace App\Models;
 
-namespace App\Models;
+    use Illuminate\Database\Eloquent\Factories\HasFactory;
+    use Illuminate\Database\Eloquent\Model;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+    class Publisher extends Model
+    {
+        use HasFactory;
+        protected $fillable = ['name', 'address'];
 
-class Publisher extends Model
-{
-    use HasFactory;
-
-    protected $fillable = ['name'];
-}
+        public function books()
+        {
+            return $this->hasMany(Book::class);
+        }
+    }
